ff8b9975f6a6e846bfd67e0a110f581d6897cf53
Tilman Niestroj <tilman@prinz-niestroj.com>
2021-03-11 11:39:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/einzelbeispiel_software_engineering_tilman_niestroj/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.einzelbeispiel_software_engineering_tilman_niestroj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
